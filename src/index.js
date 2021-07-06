e21483499028339596f23765d42761d51372a90b
kiborgok <borgokalek14@gmail.com>
2021-07-06 23:32:25 +0300

    Added sentry

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://1a371826265047879b0eda1e36630b2c@o913681.ingest.sentry.io/5851883",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
